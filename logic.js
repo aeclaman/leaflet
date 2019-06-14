c25adf99ede23fcd8fcbbe5b1c5f342ddc300688
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-06-13 20:43:19 -0500

    clean up

--- logic.js
+++ logic.js
@@ -4,17 +4,36 @@ var map = L.map("map", {
   zoom: 4
 });
 
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+// Adding tile layers
+var greyscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.light",
   accessToken: API_KEY
 }).addTo(map);
 
-// var link = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/" +
-// "35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";//all..past week
+var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-satellite",
+  accessToken: API_KEY
+});
+
+var basic = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.run-bike-hike",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Greyscale: greyscale,
+  Satellite: satellite,
+  Basic: basic
+};
+
+// Link to earthquate coordinate data
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"; //all..last 30 days
 
 var colors = ["chartreuse", "gold", "orange", "darkorange", "orangered", "firebrick"];
 
@@ -24,6 +43,7 @@ function chooseColor(magnitude) {
 }
 
 var geojson;
+var dateTime;
 
 // Grabbing our GeoJSON data
 d3.json(link, function(data) {
@@ -42,7 +62,8 @@ d3.json(link, function(data) {
       return L.circleMarker(latlng);
     }
   }).bindPopup(function (layer) {
-     return ("<h5>" + layer.feature.properties.place + "</h5><hr><h5>Magnitude: " + layer.feature.properties.mag + "</h5>");
+      dateTime = new Date(layer.feature.properties.time);
+      return ("<h5>" + layer.feature.properties.place + "</h5><hr><h5>Magnitude: " + layer.feature.properties.mag + "</h5><h5>Occured: " + dateTime.toLocaleDateString("en-US") + "</h5>");
   }).addTo(map);
 });
 
@@ -57,8 +78,11 @@ d3.json(link, function(data) {
       for (var i = 0; i < labels.length; i++) {
         list_elements.push("<li style=\"background-color: " + colors[i] + "\"></li>  " + labels[i] + "<br>");
       }
-      list_elements.push("<li><i>Amy Claman </i></li>")
+      list_elements.push("<li><i>Amy Claman </i></li>")//Add name to html per hw instructions
       div.innerHTML += "<ul>" + list_elements.join("") + "</ul>";
       return div;
   }
 legend.addTo(map);
+
+// Add the layer control to the map
+L.control.layers(baseMaps).addTo(map);
